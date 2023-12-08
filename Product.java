619ba45becd0ee1fb92df53b9062d5b79e642ac9
Aiden Anderson <153255637+goats-pager@users.noreply.github.com>
2023-12-08 11:28:11 +0700

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,21 @@
+public class Product {
+    private String productId;
+    private String productName;
+    private double price;
+
+    public Product(String productId, String productName, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    // Getter and setter methods
+
+    public String toString() {
+        return "Product{" +
+                "productId='" + productId + '\'' +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
